0f5d1dd53c3cc00884a06bf26fa20e7984a5d13d
PhillipCostas <Phillipcostas@gmail.com>
2024-06-26 11:20:21 -0400

    set up the index route to the backend

--- server.js
+++ server.js
@@ -7,19 +7,24 @@ const mongoose = require('mongoose');
 const testJWTRouter = require('./controllers/test-jwt');
 const usersRouter = require('./controllers/users');
 const profilesRouter = require('./controllers/profiles');
+const hootsRouter = require('./controllers/hoots.js');
+
 
 mongoose.connect(process.env.MONGODB_URI);
 
 mongoose.connection.on('connected', () => {
     console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
+
 app.use(cors());
 app.use(express.json());
+
 
 // Routes go here
 app.use('/test-jwt', testJWTRouter);
 app.use('/users', usersRouter);
 app.use('/profiles', profilesRouter);
+app.use('/hoots', hootsRouter);
 
 app.listen(3000, () => {
     console.log('The express app is ready!');
